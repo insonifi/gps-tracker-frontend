f609ee50d685e84e8163123a0cb41fe611e244d5
Andrey <insonifi@gmail.com>
2013-08-21 16:34:53 -0400

    set markers attribute for leaflet

--- js/controllers.js
+++ js/controllers.js
@@ -40,12 +40,6 @@ angular.module('core.controllers', [])
           lng: 24.1040,
           zoom: 12
         },
-        markers : {
-            '123' : {
-                lat: 56.873967,
-                long: 24.1189
-            }
-        },
         defaults: {
             doubleClickZoom: false,
             maxZoom: 18
