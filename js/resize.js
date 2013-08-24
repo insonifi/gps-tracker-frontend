fb0f7158f3ec64d3e26e3d03c901e2e6108b97cd
Andrey <insonifi@gmail.com>
2013-08-24 17:17:48 -0400

    introduce map containing div

--- js/resize.js
+++ js/resize.js
@@ -1,4 +1,6 @@
+
 window.onresize = function () {
-    document.body.style.width = window.innerWidth;
-    document.body.style.height = window.innerWidth;
+    var container = document.getElementById('map-container');
+    container.style.width = window.innerWidth;
+    container.style.height = window.innerWidth;
 }
