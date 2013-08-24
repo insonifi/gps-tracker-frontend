295939d1ca4adf069cd192c22cf0f346fad98a7c
Andrey <insonifi@gmail.com>
2013-08-24 18:43:38 -0400

    fix resize function

--- js/resize.js
+++ js/resize.js
@@ -1,1 +1,21 @@
-window.onresize = resizeWindow;
+window.onload = function () {
+    var checkInterval = setInterval(function () {
+        if (document.readyState === "complete") {
+            L.container = document.querySelector('.leaflet-container');
+            L.containerScope = angular.element(L.container).scope();
+            var scope = L.containerScope;
+            scope.$apply(function () {
+                scope.map_width = window.innerWidth;
+                scope.map_height = window.innerHeight;
+            });
+            clearInterval(checkInterval);
+        }
+    }, 100)
+}
+window.onresize = function () {
+    var scope = L.containerScope;
+    scope.$apply(function () {
+        scope.map_width = window.innerWidth;
+        scope.map_height = window.innerHeight;
+    });
+}
