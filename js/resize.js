fa71f132c51bba1c61a462b6caf34084477d7d33
Andrey <insonifi@gmail.com>
2013-08-24 17:37:15 -0400

    correct leaflet style

--- js/resize.js
+++ js/resize.js
@@ -1,8 +1,8 @@
 
 window.onresize = function () {
     var container = document.querySelector('.leaflet-container');
-    document.body.style.width = window.innerWidth + 'px';
-    document.body.style.height = window.innerHeight + 'px';
-    container.style.width = window.innerWidth + 'px';
-    container.style.height = window.innerHeight + 'px';
+    // document.body.style.width = window.innerWidth + 'px';
+    // document.body.style.height = window.innerHeight + 'px';
+    container.style.width = (window.innerWidth - 30) + 'px';
+    container.style.height = (window.innerHeight - 20) + 'px';
 }
