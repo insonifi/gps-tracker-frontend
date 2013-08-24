51f49b52b58fc85c01de65f7260991bb9af598f1
Andrey <insonifi@gmail.com>
2013-08-24 17:31:30 -0400

    fix resize function

--- js/resize.js
+++ js/resize.js
@@ -1,5 +1,8 @@
 
 window.onresize = function () {
+    var container = document.querySelector('.leaflet-container');
     document.body.style.width = window.innerWidth + 'px';
     document.body.style.height = window.innerHeight + 'px';
+    container.style.width = window.innerWidth + 'px';
+    container.style.height = window.innerHeight + 'px';
 }
