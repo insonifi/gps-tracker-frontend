6db501476520d14b10c70d8c3707a1c810d47322
Andrey <insonifi@gmail.com>
2013-08-24 15:34:44 -0400

    correct CSS for address class

--- js/directives.js
+++ js/directives.js
@@ -42,7 +42,7 @@ angular.module('core.directives', [])
               '<ul class="slidee">' +
                 '<li id="{{$index}}" ng-repeat="item in waypoints | period:start:end" ng-click="showAddress()">' +
                   '<div>' +
-                    '<span ng-show="item.show_address" style="padding-right: 1em">{{item.address}}</span><span>{{item.timestamp|date:"HH:mm:ss"}}</span>'+
+                    '<span ng-show="item.show_address" class="address">{{item.address}}</span><span>{{item.timestamp|date:"HH:mm:ss"}}</span>'+
                   '</div>' +
                 '</li>' +
               '</ul>' +
