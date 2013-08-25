5a14c4c95a60abb7ffceeda917a1e0a7677e1ac0
Andrey <insonifi@gmail.com>
2013-08-25 05:38:04 -0400

    when resizing take window size -15px

--- js/resize.js
+++ js/resize.js
@@ -1,13 +1,15 @@
 window.onload = function () {
-    var checkInterval = setInterval(function () {
+    var margin = 15,
+    checkInterval = setInterval(function () {
         if (document.readyState === "complete") {
-            document.body.style.width = window.innerWidth - 25 + 'px';
-            document.body.style.height = window.innerHeight - 25 + 'px';
+            document.body.style.width = window.innerWidth - margin + 'px';
+            document.body.style.height = window.innerHeight - margin + 'px';
             clearInterval(checkInterval);
         }
-    }, 100)
+    }, 100);
 }
 window.onresize = function () {
-    document.body.style.width = window.innerWidth - 25 + 'px';
-    document.body.style.height = window.innerHeight - 25 + 'px';
+    var margin = 15;
+    document.body.style.width = window.innerWidth - margin + 'px';
+    document.body.style.height = window.innerHeight - margin + 'px';
 }
