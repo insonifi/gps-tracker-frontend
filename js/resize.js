c25da54917c326e6a70949f0b1f3dc1f0a9ec205
Andrey <insonifi@gmail.com>
2013-08-25 05:32:45 -0400

    fix resizing body

--- js/resize.js
+++ js/resize.js
@@ -1,13 +1,13 @@
 window.onload = function () {
     var checkInterval = setInterval(function () {
         if (document.readyState === "complete") {
-            document.body.style.width = window.innerWidth;
-            document.body.style.height = window.innerHeight;
+            document.body.style.width = window.innerWidth + 'px';
+            document.body.style.height = window.innerHeight + 'px';
             clearInterval(checkInterval);
         }
     }, 100)
 }
 window.onresize = function () {
-    document.body.style.width = window.innerWidth;
-    document.body.style.height = window.innerHeight;
+    document.body.style.width = window.innerWidth + 'px';
+    document.body.style.height = window.innerHeight + 'px';
 }
