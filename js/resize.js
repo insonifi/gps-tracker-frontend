d2303b9b95ee8f9cd976e77bbc09463570bf207a
Andrey <insonifi@gmail.com>
2013-08-25 05:31:55 -0400

    resize body

--- js/resize.js
+++ js/resize.js
@@ -1,15 +1,13 @@
 window.onload = function () {
     var checkInterval = setInterval(function () {
         if (document.readyState === "complete") {
-            L.container = document.querySelector('.leaflet-container');
-            L.container.style.position = 'absolute';
-            L.container.style.width = (window.innerWidth - 30) + 'px';
-            L.container.style.height = (window.innerHeight - 20) + 'px';
+            document.body.style.width = window.innerWidth;
+            document.body.style.height = window.innerHeight;
             clearInterval(checkInterval);
         }
     }, 100)
 }
 window.onresize = function () {
-    L.container.style.width = (window.innerWidth - 30) + 'px';
-    L.container.style.height = (window.innerHeight - 20) + 'px';
+    document.body.style.width = window.innerWidth;
+    document.body.style.height = window.innerHeight;
 }
