9639798e4a27ae499446221fdd96753415a685b1
Andrey <insonifi@gmail.com>
2013-08-28 14:11:54 -0400

    add kendo.directives to app dependencies

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('core', ['core.filters', 'core.services', 'core.directives', 'core.controllers', 'btford.socket-io', 'leaflet-directive'])
+angular.module('core', ['core.filters', 'core.services', 'core.directives', 'core.controllers', 'btford.socket-io', 'leaflet-directive', 'kendo.directives'])
