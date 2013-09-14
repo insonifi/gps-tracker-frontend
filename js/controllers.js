b6a478434aed1f35e46a0802b8a9227d92126c60
Andrey <insonifi@gmail.com>
2013-09-14 06:56:50 -0400

    show realtime waypoint update

--- js/controllers.js
+++ js/controllers.js
@@ -3,150 +3,152 @@
 /* Controllers */
 
 angular.module('core.controllers', [])
-  .controller('clockCtrl', ['$scope', 'socket',function ($scope, socket) {
-    socket.on('clock', function (time) {
-      $scope.time = time;
-    });
-  }])
-  .controller('queryCtrl', function ($scope, socket) {
-    $scope.end_date = new Date();
-    $scope.start_date = new Date($scope.end_date - 1000 * 3600 * 24);
-    socket.on('connect', function () {
-      socket.emit('get-modulelist');
-    });
-    socket.on('modulelist', function (list) {
-      $scope.list = list;
-    })
-    $scope.sendQueryRequest = function () {
-      if (!$scope.module) {
-        console.log('[queryCtrl] no module selected');
-        return;
-      }
-    var start_date = $scope.start_date,
-    end_date = $scope.end_date,
-    module_id = $scope.module.module_id;
-      
-      console.log('[queryCtrl] query %s: from %s to %s', module_id, start_date, end_date);
-        socket.emit('query-period', {module_id: module_id, start: start_date.valueOf(), end: end_date.valueOf()});
-    }
-  })
-  .controller('mapCtrl', ['$scope', 'socket', function ($scope, socket) {
-    var now,
-        trip_idx,
-        first,
-        last,
-        parking_time = 300 * 1000, /* 5mins */
-        init_vars = function () {
-            now = (new Date()).valueOf();
-            trip_idx = 0;
-            $scope.waypoints = [];
-            $scope.trips = [];
-        };
-    Date.prototype.toMyString = function () {
-        return this.getDate() + '.' 
-            + this.getMonth() + '. '
-            + this.toTimeString().slice(0, 5);
-    }
-    angular.extend($scope, {
-        riga: {
-          lat: 56.9496,
-          lng: 24.1040,
-          zoom: 12
-        },
-        markers: {},
-        paths: {},
-        defaults: {
-            doubleClickZoom: false,
-            maxZoom: 18
-        }
-    });
-    /* Initialise variables */
-    init_vars();
-    $scope.notReceiving = true;
-    /* Query waypoints */
-    socket.on('query-waypoint', function (waypoint) {
-        var overall;
-        if($scope.notReceiving) {
-            init_vars()
-            $scope.notReceiving = false;
-            $scope.trips[0] = {};
-            $scope.trips[0].addressA = '';
-            $scope.trips[0].addressB = '';
-            $scope.trips[0].start = now;
-            $scope.trips[0].end = now;
-        }
-        /* Receive waypoint */
-        overall = $scope.trips[0];
-        overall.start = Math.min(waypoint.timestamp, overall.start);
-        overall.end = Math.max(waypoint.timestamp, overall.end);
-        waypoint.timestamp = new Date(waypoint.timestamp); /* convert to date */
-        waypoint.show_address = false;
-        waypoint.address = null;
-        $scope.waypoints.push(waypoint);
-    });
-    socket.on('query-end', function (count) {
-        console.log('Found', count, 'waypoints');
-        $scope.notReceiving = true;
-        /* Sort waypoints */
-        $scope.waypoints.sort(function (a, b) {
-            if (a.timestamp > b.timestamp) {
-                return 1;
-            }
-            if (a.timestamp < b.timestamp) {
-                return -1;
-            }
-            return 0;
+    .controller('queryCtrl', function ($scope, socket) {
+        $scope.end_date = new Date();
+        $scope.start_date = new Date($scope.end_date - 1000 * 3600 * 24);
+        socket.on('connect', function () {
+          socket.emit('get-modulelist');
         });
-        /* Detect trip */
-        (function () {
-            var i,
-                trip_idx = 1,
-                previous,
-                current,
-                now = (new Date()).valueOf(),
-                length = $scope.waypoints.length;
-            /* set start boundary */
-            $scope.trips[0].addressA = $scope.waypoints[0].timestamp.toMyString();
-            /* iterate trough waypoints */
-            for (i = 0; i < length; i += 1) {
-                previous = ($scope.waypoints[i - 1] || $scope.waypoints[i]).timestamp;
-                current = $scope.waypoints[i].timestamp;
-                if (current - previous > parking_time) {
-                    if ($scope.trips[trip_idx].end === $scope.trips[trip_idx].start) {
-                        continue;
-                    }
-                    $scope.trips[trip_idx].end = previous;
-                    $scope.trips[trip_idx].addressB = current.toMyString();
-                    trip_idx += 1;
-                }
-                if (!$scope.trips[trip_idx]) {
-                    $scope.trips[trip_idx] = {
-                        start: current,
-                        end: 0,
-                        addressA: current.toMyString()
-                    };
-                }
+        socket.on('modulelist', function (list) {
+          $scope.list = list;
+        })
+        $scope.sendQueryRequest = function () {
+          if (!$scope.module) {
+            console.log('[queryCtrl] no module selected');
+            return;
+          }
+        var start_date = $scope.start_date,
+        end_date = $scope.end_date,
+        module_id = $scope.module.module_id;
+          
+          console.log('[queryCtrl] query %s: from %s to %s', module_id, start_date, end_date);
+            socket.emit('query-period', {module_id: module_id, start: start_date.valueOf(), end: end_date.valueOf()});
+        }
+    })
+    .controller('mapCtrl', ['$scope', 'socket', function ($scope, socket) {
+        var now,
+            trip_idx,
+            first,
+            last,
+            parking_time = 300 * 1000, /* 5mins */
+            init_vars = function () {
+                now = (new Date()).valueOf();
+                trip_idx = 0;
+                $scope.waypoints = [];
+                $scope.trips = [];
+            };
+        Date.prototype.toMyString = function () {
+            return this.getDate() + '.' 
+                + this.getMonth() + '. '
+                + this.toTimeString().slice(0, 5);
+        }
+        angular.extend($scope, {
+            riga: {
+              lat: 56.9496,
+              lng: 24.1040,
+              zoom: 12
+            },
+            markers: {},
+            paths: {},
+            defaults: {
+                doubleClickZoom: false,
+                maxZoom: 18
             }
-            /* Append last waypoint */
-            $scope.trips[trip_idx].end = current;
-            $scope.trips[trip_idx].addressB = current.toMyString();
-            /* set end boundary */
-            $scope.trips[0].addressB = current.toMyString();
-            console.log('Detected', $scope.trips.length - 1, 'trips');
-            $scope.$digest();
-        }) ();
-        $scope.$broadcast('refresh-trips');
-    });
-    /* Get address for coordinates */
-    $scope.requestAddress = function (coords) {
-        socket.emit('get-address', coords);
-    };
-    socket.on('result-address', function (response) {
-        angular.forEach($scope.waypoints, function (waypoint, index) {
-            if (waypoint.lat === response.lat
-              || waypoint.long === response.long) {
-                waypoint.address = response.address;
-              }
+        });
+        /* Initialise variables */
+        init_vars();
+        $scope.notReceiving = true;
+        /* Query waypoints */
+        socket.on('query-waypoint', function (waypoint) {
+            var overall;
+            if($scope.notReceiving) {
+                init_vars()
+                $scope.notReceiving = false;
+                $scope.trips[0] = {};
+                $scope.trips[0].addressA = '';
+                $scope.trips[0].addressB = '';
+                $scope.trips[0].start = now;
+                $scope.trips[0].end = now;
+            }
+            /* Receive waypoint */
+            overall = $scope.trips[0];
+            overall.start = Math.min(waypoint.timestamp, overall.start);
+            overall.end = Math.max(waypoint.timestamp, overall.end);
+            waypoint.timestamp = new Date(waypoint.timestamp); /* convert to date */
+            waypoint.show_address = false;
+            waypoint.address = null;
+            $scope.waypoints.push(waypoint);
+        });
+        socket.on('query-end', function (count) {
+            console.log('Found', count, 'waypoints');
+            $scope.notReceiving = true;
+            /* Sort waypoints */
+            $scope.waypoints.sort(function (a, b) {
+                if (a.timestamp > b.timestamp) {
+                    return 1;
+                }
+                if (a.timestamp < b.timestamp) {
+                    return -1;
+                }
+                return 0;
+            });
+            /* Detect trip */
+            (function () {
+                var i,
+                    trip_idx = 1,
+                    previous,
+                    current,
+                    now = (new Date()).valueOf(),
+                    length = $scope.waypoints.length;
+                /* set start boundary */
+                $scope.trips[0].addressA = $scope.waypoints[0].timestamp.toMyString();
+                /* iterate trough waypoints */
+                for (i = 0; i < length; i += 1) {
+                    previous = ($scope.waypoints[i - 1] || $scope.waypoints[i]).timestamp;
+                    current = $scope.waypoints[i].timestamp;
+                    if (current - previous > parking_time) {
+                        if ($scope.trips[trip_idx].end === $scope.trips[trip_idx].start) {
+                            continue;
+                        }
+                        $scope.trips[trip_idx].end = previous;
+                        $scope.trips[trip_idx].addressB = current.toMyString();
+                        trip_idx += 1;
+                    }
+                    if (!$scope.trips[trip_idx]) {
+                        $scope.trips[trip_idx] = {
+                            start: current,
+                            end: 0,
+                            addressA: current.toMyString()
+                        };
+                    }
+                }
+                /* Append last waypoint */
+                $scope.trips[trip_idx].end = current;
+                $scope.trips[trip_idx].addressB = current.toMyString();
+                /* set end boundary */
+                $scope.trips[0].addressB = current.toMyString();
+                console.log('Detected', $scope.trips.length - 1, 'trips');
+                $scope.$digest();
+            }) ();
+            $scope.$broadcast('refresh-trips');
+        });
+        /* Get address for coordinates */
+        $scope.requestAddress = function (coords) {
+            socket.emit('get-address', coords);
+        };
+        socket.on('result-address', function (response) {
+            angular.forEach($scope.waypoints, function (waypoint, index) {
+                if (waypoint.lat === response.lat
+                    || waypoint.long === response.long) {
+                    waypoint.address = response.address;
+                }
             });
         });
+        socket.on('update-waypoint', function (waypoint) {
+            $scope.markers['now'] = {
+                    lat: waypoint.lat,
+                    lng: waypoint.long
+                }
+            }
+        })
     }])
