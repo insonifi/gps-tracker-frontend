dbf8b2c950c3b982d2676889d614260bfaf777e0
Andrey <insonifi@gmail.com>
2013-09-14 17:09:07 -0400

    remove limit, doesn't seem to improve anything. need to look for other optimisation

--- js/path.js
+++ js/path.js
@@ -1,11 +1,10 @@
 onmessage = function (event) {
-    var path = [],
-        limit = 100; /* limit path waypoints */
+    var path = [];
     path = event.data.map(function (item) {
         return {
             lat: item.lat,
             lng: item.long,
         }
     });
-    postMessage(path.slice(0, limit));
+    postMessage(path);
 }
