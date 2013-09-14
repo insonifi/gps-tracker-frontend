a90c00065901b3efb8a8d083d87e62a78101034d
Andrey <insonifi@gmail.com>
2013-09-14 17:53:51 -0400

    work on waypoints processing workers

--- js/path.js
+++ js/path.js
@@ -1,10 +1,8 @@
 onmessage = function (event) {
-    var path = [];
-    path = event.data.map(function (item) {
+    postMessage(event.data.map(function (item) {
         return {
             lat: item.lat,
             lng: item.long,
         }
-    });
-    postMessage(path);
+    }));
 }
--- js/range.js
+++ js/range.js
@@ -1,9 +1,8 @@
 onmessage = function (event) {
     var start = event.data.start,
         end = event.data.end,
-        waypoints = event.data.waypoints,
-        range = waypoints.filter(function (item) {
-            return (item.timestamp >= start && item.timestamp <= end);
-        });
-    postMessage(range);
+        waypoints = event.data.waypoints;
+    postMessage(waypoints.filter(function (item) {
+        return (item.timestamp >= start && item.timestamp <= end);
+    }));
 }
