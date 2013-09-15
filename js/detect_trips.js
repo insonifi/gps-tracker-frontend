bdf5cd830d932781374226cc79e25bb6c5e34134
Andrey <insonifi@gmail.com>
2013-09-15 04:33:56 -0400

    work on distance display in trips-list directive

--- js/detect_trips.js
+++ js/detect_trips.js
@@ -3,7 +3,7 @@ importScripts('distance.js');
 Date.prototype.toMyString = function () {
     return this.getDate() + '.' 
         + (this.getMonth() + 1) + '. '
-        + this.toLocaleTimeString();
+        + this.toLocaleTimeString().slice(0,5);
 }
 onmessage = function (event) {
     var i,
@@ -36,7 +36,7 @@ onmessage = function (event) {
             }
             trips[trip_idx].end = previous;
             trips[trip_idx].addressB = current.toMyString();
-            trips[trip_idx].distance += calculateDistance(previous_coords, current_coords);
+            trips[trip_idx].distance += calculateDistance(previous_coords, current_coords).distance;
             trips[0].distance = trips[trip_idx].distance;
             trip_idx += 1;
         }
@@ -52,7 +52,7 @@ onmessage = function (event) {
     /* Append last waypoint */
     trips[trip_idx].end = current;
     trips[trip_idx].addressB = current.toMyString();
-    trips[trip_idx].distance += calculateDistance(previous_coords, current_coords);
+    trips[trip_idx].distance += calculateDistance(previous_coords, current_coords).distance;
     trips[0].distance = trips[trip_idx].distance;
     /* set end boundary */
     trips[0].addressB = current.toMyString();
