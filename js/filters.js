0009c96dbab81e632d696ab84df15812042f27eb
Andrey <insonifi@gmail.com>
2013-09-15 05:11:58 -0400

    mistype

--- js/filters.js
+++ js/filters.js
@@ -2,15 +2,15 @@
 
 /* Filters */
 
-angular.module('core.filters', []).
-  filter('period', function() {
+angular.module('core.filters', [])
+  .filter('period', function() {
     return function(list, start, end) {
       return list.filter(function (item) {
           return (item.timestamp >= start && item.timestamp <= end);
       })
     }
   })
-  filter('km', function () {
+  .filter('km', function () {
       return function (meters) {
           return (meters / 1000).toFixed(1)
       }
