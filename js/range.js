5bed5a0a630eb53a84f5e97204495afd7ddd6db5
Andrey <insonifi@gmail.com>
2013-09-22 13:32:14 -0400

    convert ArrayBuffer from range Worker back to array

--- js/range.js
+++ js/range.js
@@ -18,7 +18,7 @@ jsonToArrayBuffer = function (json) {
 onmessage = function (event) {
     var start = event.data.start,
         end = event.data.end,
-        waypoints = event.data.waypoints;
+        waypoints = arrayBufferToJSON(event.data.waypoints);
     postMessage(waypoints.filter(function (item) {
         return (item.timestamp >= start && item.timestamp <= end);
     }));
