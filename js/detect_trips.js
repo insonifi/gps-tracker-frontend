bf9a007b6ac5d2881fc75fba7747c7655334afc5
Andrey <insonifi@gmail.com>
2013-09-22 11:43:06 -0400

    fix toMyString call

--- js/detect_trips.js
+++ js/detect_trips.js
@@ -61,7 +61,7 @@ self.onmessage = function (event) {
                 continue;
             }
             trips[trip_idx].end = previous;
-            trips[trip_idx].addressB = current.toMyString();
+            trips[trip_idx].addressB = toMyString(current);
             trips[trip_idx].distance += calculateDistance(previous_coords, current_coords);
             trips[0].distance += trips[trip_idx].distance;
             trip_idx += 1;
