69fdd27c058d019bb6a3220d7ded1d2f729f935d
Andrey <insonifi@gmail.com>
2013-09-29 10:55:52 -0400

    work on SlickGrid

--- js/directives.js
+++ js/directives.js
@@ -79,7 +79,7 @@ angular.module('core.directives', [])
             '<div class="grid"></div>',
         scope: true,
         controller: ['$scope', '$rootScope', function ($scope, $root) {
-            $scope.waypointsOptions = {data: $scope.waypoints_range};
+            $scope.waypointsOptions = [];
                 
             $scope.$on('refresh-waypoints', function (event, start, end, startIdx, endIdx) {
                 $scope.start = start;
