c2a723beec25677cffb2cc4618a58ccffea8ec50
Andrey <insonifi@gmail.com>
2013-10-05 09:38:43 -0400

    rearrange code\nmove connection related code to 'cnxn' service

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,7 @@ angular.module('core.controllers', [])
     .controller('queryCtrl', ['$scope', '$rootScope', 'cnxn', function ($scope, $root, cnxn) {
         $scope.end_date = new Date();
         $scope.start_date = new Date($scope.end_date - 1000 * 3600 * 24);
-        
+        $scope.list = [];
         $scope.$on('modulelist', function (list) {
             $scope.list = list;    
         });
