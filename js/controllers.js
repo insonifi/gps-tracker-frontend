8864d305587929cbe3be017fd5226a65909ed175
Andrey <insonifi@gmail.com>
2013-10-05 11:02:39 -0400

    rename again to reset_vars

--- js/controllers.js
+++ js/controllers.js
@@ -26,7 +26,7 @@ angular.module('core.controllers', [])
             $root.$broadcast('msg', 'Searching between', start_date.toLocaleString(), '...', end_date.toLocaleString(), 'for module', module_id);
             cnxn.queryPeriod(module_id, start_date, end_date, chunk_size);
         }
-        $scope.clear = function () {
+        $scope.reset_vars = function () {
             $root.waypoints = [];
             $root.trips = [];
             $root.$digest();
