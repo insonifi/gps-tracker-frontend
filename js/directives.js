f48e1eab60ded825b4cb163bfcc5d5f2b2c96075
Andrey <insonifi@gmail.com>
2013-11-11 16:25:17 -0500

    reinitialize grid only if waypoints list is not empty

--- js/directives.js
+++ js/directives.js
@@ -131,11 +131,11 @@ angular.module('core.directives', [])
                         };
                         /* filter waypoints*/
                         $scope.message(newValue.length, 'waypoints', 3);
+                        $scope.grid.setData(newValue, true);
+                        $scope.grid.invalidate();
+                        $scope.grid.resetActiveCell()
+                        $scope.selectPath();
                     }
-                    $scope.grid.setData(newValue, true);
-                    $scope.grid.invalidate();
-                    $scope.grid.resetActiveCell()
-                    $scope.selectPath();
                     /*$scope.$root.$broadcast('select-waypoint', null);*/
                 }
             })
