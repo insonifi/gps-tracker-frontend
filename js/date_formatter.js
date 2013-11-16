c5b7e5273172b0087a42893556fd6f83a356bb1b
Andrey <insonifi@gmail.com>
2013-11-16 12:43:30 -0500

    format seconds as 2-digits

--- js/date_formatter.js
+++ js/date_formatter.js
@@ -1,4 +1,4 @@
 var dateFormatter = function (row, cell, value, columnDef, dataContext) {
     var date = new Date(value)
-    return date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + date.getSeconds();
+    return date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
 }
