322fd706d28d31aa3abea3459bacc9e156d9b82a
Andrey <insonifi@gmail.com>
2013-12-14 08:04:20 -0500

    apply address value after promise is resolved

--- js/directives.js
+++ js/directives.js
@@ -265,16 +265,12 @@ angular.module('core.directives', [])
                         len = trips.length;
                         
                     for (i = 1; i < len; i += 1) {
-                        cnxn.requestAddress(waypoints[trips[i].idx_start]).then(function (value) {
-                            $scope.$apply(function () {
-                                trips[i].address_start = value;  
-                            })
+                        trips[i].address_start = cnxn.requestAddress(waypoints[trips[i].idx_start]).then(function (value) {
+                            trips[i].address_start = value;
                         });
-                        cnxn.requestAddress(waypoints[trips[i].idx_end].then(function (value) {
-                            $scope.$apply(function () {
-                                trips[i].address_end = value;
-                            })
-                        }));
+                        trips[i].address_end = cnxn.requestAddress(waypoints[trips[i].idx_end]).then(function (value) {
+                            trips[i].address_end = value;
+                        });;
                     }
                 }
             })
